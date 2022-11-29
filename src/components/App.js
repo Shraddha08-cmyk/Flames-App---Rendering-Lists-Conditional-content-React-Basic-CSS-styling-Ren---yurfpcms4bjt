2ea8a09c8c689ad4ab5d5e54f8159675951e5fae
Shraddha08-cmyk <shraddhatiwari8175@gmail.com>
2022-11-30 01:08:53 +0530

    new

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
-}
+class App extends Component{
 
+   render(){ 
 
-export default App;
+    
+    const handleSubmit = (event) =>{
+       
+        var name1 = document.getElementById("name1").value;
+        var name2 = document.getElementById("name2").value;
+        var result = document.getElementById("result");
+
+        if(name1.length == 0 || name2.length == 0){
+
+            result.innerHTML="Please Enter valid input";
+            return;
+
+        }
